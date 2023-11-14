9c5a60741f8a4fd3a51030e3a99f446acd5c28dd
Nikola Hristov <nikola@nikolahristov.tech>
2023-11-14 22:41:28 +0200

    squash!

--- Source/liveShare/service.ts
+++ Source/liveShare/service.ts
@@ -4,65 +4,61 @@
 import { Uri } from "vscode";
 import { LiveShare, Role, SharedService, SharedServiceProxy } from "vsls";
 import {
-	endCurrentCodeTour,
-	onDidEndTour,
-	onDidStartTour,
-	startCodeTour,
+  endCurrentCodeTour,
+  onDidEndTour,
+  onDidStartTour,
+  startCodeTour
 } from "../store/actions";
 
 interface Message {
-	data?: any;
-	peer?: number;
+  data?: any;
+  peer?: number;
 }
 
 const TOUR_ENDED_NOTIFICATION = "tourEnded";
 const TOUR_STARTED_NOTIFICATION = "tourStarted";
 
 export default function (
-	api: LiveShare,
-	service: SharedService | SharedServiceProxy
+  api: LiveShare,
+  service: SharedService | SharedServiceProxy
 ) {
-	const peer = api.session.peerNumber;
+  const peer = api.session.peerNumber;
 
-	onDidEndTour(() => {
-		service.notify(TOUR_ENDED_NOTIFICATION, { peer });
-	});
+  onDidEndTour(() => {
+    service.notify(TOUR_ENDED_NOTIFICATION, { peer });
+  });
 
-	service.onNotify(TOUR_ENDED_NOTIFICATION, (message: Message) => {
-		if (message.peer === peer) return;
+  service.onNotify(TOUR_ENDED_NOTIFICATION, (message: Message) => {
+    if (message.peer === peer) return;
 
-		endCurrentCodeTour(false);
-		service.notify(TOUR_ENDED_NOTIFICATION, message);
-	});
+    endCurrentCodeTour(false);
+    service.notify(TOUR_ENDED_NOTIFICATION, message);
+  });
 
-	onDidStartTour(([tour, stepNumber]) => {
-		const newTour = { ...tour };
+  onDidStartTour(([tour, stepNumber]) => {
+    const newTour = { ...tour };
 
-		if (api.session.role === Role.Host) {
-			newTour.id = api
-				.convertLocalUriToShared(Uri.parse(tour.id))
-				.toString();
-		} else {
-			newTour.id = api
-				.convertSharedUriToLocal(Uri.parse(tour.id))
-				.toString();
-		}
+    if (api.session.role === Role.Host) {
+      newTour.id = api.convertLocalUriToShared(Uri.parse(tour.id)).toString();
+    } else {
+      newTour.id = api.convertSharedUriToLocal(Uri.parse(tour.id)).toString();
+    }
 
-		const message = {
-			peer,
-			data: {
-				tour: newTour,
-				stepNumber,
-			},
-		};
+    const message = {
+      peer,
+      data: {
+        tour: newTour,
+        stepNumber
+      }
+    };
 
-		service.notify(TOUR_STARTED_NOTIFICATION, message);
-	});
+    service.notify(TOUR_STARTED_NOTIFICATION, message);
+  });
 
-	service.onNotify(TOUR_STARTED_NOTIFICATION, (message: Message) => {
-		if (message.peer === peer) return;
+  service.onNotify(TOUR_STARTED_NOTIFICATION, (message: Message) => {
+    if (message.peer === peer) return;
 
-		startCodeTour(message.data.tour, message.data.stepNumber);
-		service.notify(TOUR_STARTED_NOTIFICATION, message);
-	});
+    startCodeTour(message.data.tour, message.data.stepNumber);
+    service.notify(TOUR_STARTED_NOTIFICATION, message);
+  });
 }
