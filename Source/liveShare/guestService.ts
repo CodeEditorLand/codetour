da68188a87e268b55002534ba29044e728049f66
Nikola Hristov <nikola@nikolahristov.tech>
2023-11-17 16:18:59 +0200

    squash!

--- Source/liveShare/guestService.ts
+++ Source/liveShare/guestService.ts
@@ -7,13 +7,13 @@ import { startCodeTour } from "../store/actions";
 import initializeBaseService from "./service";
 
 export async function initializeService(vslsApi: vsls.LiveShare) {
-  const service = await vslsApi.getSharedService(EXTENSION_NAME);
-  if (!service) return;
+	const service = await vslsApi.getSharedService(EXTENSION_NAME);
+	if (!service) return;
 
-  const response = await service.request("getCurrentTourStep", []);
-  if (response) {
-    startCodeTour(response.tour, response.stepNumber);
-  }
+	const response = await service.request("getCurrentTourStep", []);
+	if (response) {
+		startCodeTour(response.tour, response.stepNumber);
+	}
 
-  initializeBaseService(vslsApi, service);
+	initializeBaseService(vslsApi, service);
 }
